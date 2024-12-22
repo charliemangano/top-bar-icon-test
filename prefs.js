d607ca33dcd6d715141ac0ff145a9d80834b8cb9
charlie mangano <eva.mangano@epfl.ch>
2024-12-22 20:20:26 +0100

    feat: add `buildPrefsWidget` function to `prefs.js`

--- prefs.js
+++ prefs.js
@@ -1,3 +1,18 @@
+const { GObject, Gtk } = imports.gi;
+
 function init () {}
+
+function buildPrefsWidget() {
+	let widget = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, spacing: 10 });
+
+	// preference widgets
+	let label = new Gtk.Label({ label: "Example Preference" });
+	widget.add(label);
+
+	widget.show_all();
+	return widget;
+}
+
 function enable () {}
+
 function disable() {}
